9342df4c346ffd9761dbe00f4d0b5f170bf9f642
App Academy Student <student@appacademy.io>
2015-08-28 16:26:06 -0700

    refactoring comments

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,16 +1,20 @@
 require 'yaml'
 require 'byebug'
+
+# Break into multiple files
 class MineSweeper
   attr_accessor :user, :board
 
   def initialize(user = User.new, board = Board.new)
     @user = user
     @board = board
+    board.populate_grid
+    debugger
   end
 
   def play
-    board.populate_grid
 
+    #Refactor into helper methods
     until board.won?
       system("clear")
       board.display
@@ -19,6 +23,7 @@ class MineSweeper
 
       action, x, y = move[0], move[1].to_i, move[2].to_i
 
+      # Refactor to case
       if action == "flag"
         board.flag_bomb(x, y)
       elsif action == "unflag"
@@ -36,6 +41,8 @@ class MineSweeper
     user.take_turn
   end
 
+
+  # Move some functionality to be private
   def save
     puts "Enter title:"
     name = gets.chomp
@@ -68,11 +75,10 @@ class Board
     @size = size
   end
 
+  # Add flagged_bomb
   def won?
-    grid.all? do |row|
-      row.all? do |tile|
-        tile.bomb ? tile.flagged : true
-      end
+    grid.flatten.all? do |tile|
+      (tile.bomb && tile.flagged) || tile.state == :up
     end
   end
 
@@ -80,8 +86,10 @@ class Board
     Kernel.abort("You lose!")
   end
 
+
+  # Refactor
   def reveal(row,col)
-    return lose if self[row,col].bomb == true
+    lose if self[row,col].bomb == true
     return if self[row,col].state == :up
     self[row,col].state = :up
 
@@ -90,13 +98,18 @@ class Board
     end
   end
 
+
+  # Refactor to be more concise and readable
   def display
     grid.each_with_index do |row, ridx|
       display_rows = row.each_with_index.map do |col, cidx|
         tile_spot = self[ridx, cidx]
+
+        # Allow tile to be responsible for it's representation (override to_s)
         if tile_spot.state == :down
           tile_spot.flagged ? :F : :*
         else
+          # Refactor
           num_of_bombs = neighbors(ridx, cidx).inject(0) do |accum, neighbor|
             self[*neighbor].bomb ? accum + 1 : accum
           end
@@ -135,21 +148,24 @@ class Board
       [row + 1, col + 1]
     ]
 
+  #  positions.select(&:on_board)
     positions.select do |tile|
       row, col = tile[0], tile[1]
       [row, col].all? { |pos| pos >= 0 && pos < size }
     end
   end
 
+  #each_index
   def populate_grid
     bombs = bomb_order
-    grid.each_with_index do |row, ridx|
-      row.each_with_index do |col, cidx|
-        grid[ridx][cidx] = Tile.new(bombs.pop)
+    grid.each_with_index do |row, row_idx|
+      row.each_with_index do |col, col_idx|
+        grid[row_idx][col_idx] = Tile.new(bombs.pop)
       end
     end
   end
 
+  # Renamed to be more semantically meaningful
   def bomb_order
     bombs = []
     number_bombs.times do
